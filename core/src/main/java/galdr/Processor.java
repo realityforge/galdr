f3cf95305e390f428289eb6841658cde3553be9e
Peter Donald <peter@realityforge.org>
2019-09-12 12:38:37 +1000

    Add base processor class

---
+++ core/src/main/java/galdr/Processor.java
@@ -0,0 +1,26 @@
+package galdr;
+
+import java.util.Objects;
+import javax.annotation.Nonnull;
+
+/**
+ * Base processor class.
+ */
+public abstract class Processor
+{
+  @Nonnull
+  private final World _world;
+
+  protected Processor( @Nonnull final World world )
+  {
+    _world = Objects.requireNonNull(world);
+  }
+
+  public abstract void process(float timeDelta);
+
+  @Nonnull
+  protected final World getWorld()
+  {
+    return _world;
+  }
+}
