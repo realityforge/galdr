57c7522cf26e8006ff16d1fa61048cffa88b483a
Peter Donald <peter@realityforge.org>
2019-09-14 13:17:37 +1000

    Update the world to contain ComponentRegistry

--- core/src/main/java/galdr/World.java
+++ core/src/main/java/galdr/World.java
@@ -1,5 +1,27 @@
 package galdr;
 
+import java.util.Set;
+import javax.annotation.Nonnull;
+
 public final class World
 {
+  @Nonnull
+  private final ComponentRegistry _componentRegistry;
+
+  World( @Nonnull final ComponentManager<?>... components )
+  {
+    _componentRegistry = new ComponentRegistry( components );
+  }
+
+  @Nonnull
+  Set<Class<?>> getComponentTypes()
+  {
+    return _componentRegistry.getComponentTypes();
+  }
+
+  @Nonnull
+  public <T> ComponentAPI<T> getComponentByType( @Nonnull final Class<T> type )
+  {
+    return _componentRegistry.getComponentManagerByType( type ).getApi();
+  }
 }
---
+++ core/src/test/java/galdr/WorldTest.java
@@ -0,0 +1,44 @@
+package galdr;
+
+import java.util.Set;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class WorldTest
+  extends AbstractTest
+{
+  private static class Component1
+  {
+  }
+
+  private static class Component2
+  {
+  }
+
+  @Test
+  public void getComponentByType()
+  {
+    final ComponentManager<Component1> componentManager =
+      new FastArrayComponentManager<>( Component1.class, Component1::new );
+    final World world = new World( componentManager );
+
+    final ComponentAPI<Component1> api = world.getComponentByType( Component1.class );
+    assertNotNull( api );
+    assertEquals( api, componentManager.getApi() );
+  }
+
+  @Test
+  public void getComponentTypes()
+  {
+    final ComponentManager<Component1> componentManager1 =
+      new FastArrayComponentManager<>( Component1.class, Component1::new );
+    final ComponentManager<Component2> componentManager2 =
+      new FastArrayComponentManager<>( Component2.class, Component2::new );
+    final World world = new World( componentManager1, componentManager2 );
+
+    final Set<Class<?>> componentTypes = world.getComponentTypes();
+    assertEquals( componentTypes.size(), 2 );
+    assertTrue( componentTypes.contains( Component1.class ) );
+    assertTrue( componentTypes.contains( Component2.class ) );
+  }
+}
