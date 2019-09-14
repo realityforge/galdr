8bf1ae68a57f6ace73e5e169bb34d7de75cb5733
Peter Donald <peter@realityforge.org>
2019-09-14 12:15:20 +1000

    Rename variables

--- core/src/test/java/galdr/FastArrayComponentManagerTest.java
+++ core/src/test/java/galdr/FastArrayComponentManagerTest.java
@@ -15,41 +15,41 @@ public class FastArrayComponentManagerTest
   public void basicOperation()
   {
     final Supplier<Component1> createFn = Component1::new;
-    final FastArrayComponentManager<Component1> componentStore =
+    final FastArrayComponentManager<Component1> componentManager =
       new FastArrayComponentManager<>( Component1.class, createFn, 5 );
 
-    assertEquals( componentStore.toString(), "ComponentManager[Component1=-1]" );
-    componentStore.initIndex( 23 );
-    assertEquals( componentStore.getIndex(), 23 );
+    assertEquals( componentManager.toString(), "ComponentManager[Component1=-1]" );
+    componentManager.initIndex( 23 );
+    assertEquals( componentManager.getIndex(), 23 );
 
-    assertEquals( componentStore.getType(), Component1.class );
-    assertEquals( componentStore.getCreateFn(), createFn );
-    assertEquals( componentStore.getName(), "Component1" );
-    assertEquals( componentStore.toString(), "ComponentManager[Component1=23]" );
-    assertEquals( componentStore.hashCode(), 23 );
-    assertEquals( componentStore.capacity(), 5 );
+    assertEquals( componentManager.getType(), Component1.class );
+    assertEquals( componentManager.getCreateFn(), createFn );
+    assertEquals( componentManager.getName(), "Component1" );
+    assertEquals( componentManager.toString(), "ComponentManager[Component1=23]" );
+    assertEquals( componentManager.hashCode(), 23 );
+    assertEquals( componentManager.capacity(), 5 );
 
     final int entityId = 9;
 
     // entityId is > initial capacity
-    assertFalse( componentStore.has( entityId ) );
-    assertNull( componentStore.find( entityId ) );
+    assertFalse( componentManager.has( entityId ) );
+    assertNull( componentManager.find( entityId ) );
 
-    final Component1 component = componentStore.create( entityId );
+    final Component1 component = componentManager.create( entityId );
     assertNotNull( component );
 
-    assertEquals( componentStore.capacity(), 13 );
+    assertEquals( componentManager.capacity(), 13 );
 
-    assertTrue( componentStore.has( entityId ) );
-    assertEquals( componentStore.find( entityId ), component );
-    assertEquals( componentStore.get( entityId ), component );
+    assertTrue( componentManager.has( entityId ) );
+    assertEquals( componentManager.find( entityId ), component );
+    assertEquals( componentManager.get( entityId ), component );
 
-    componentStore.remove( entityId );
+    componentManager.remove( entityId );
 
-    assertFalse( componentStore.has( entityId ) );
-    assertNull( componentStore.find( entityId ) );
+    assertFalse( componentManager.has( entityId ) );
+    assertNull( componentManager.find( entityId ) );
 
     // Capacity is not dminished after remove
-    assertEquals( componentStore.capacity(), 13 );
+    assertEquals( componentManager.capacity(), 13 );
   }
 }
