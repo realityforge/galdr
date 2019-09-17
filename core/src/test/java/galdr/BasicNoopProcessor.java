0f92ecf9aa7ef5c3f765bf749a8f460dc0124396
Peter Donald <peter@realityforge.org>
2019-09-18 09:10:39 +1000

    Remove unnecessary constructor

--- core/src/test/java/galdr/BasicNoopProcessor.java
+++ core/src/test/java/galdr/BasicNoopProcessor.java
@@ -1,20 +1,8 @@
 package galdr;
 
-import javax.annotation.Nonnull;
-
 final class BasicNoopProcessor
   extends Processor
 {
-  BasicNoopProcessor()
-  {
-    this( "BasicNoopProcessor" );
-  }
-
-  BasicNoopProcessor( @Nonnull final String name )
-  {
-    super( Galdr.areNamesEnabled() ? name : null );
-  }
-
   @Override
   protected void process( final int delta )
   {
