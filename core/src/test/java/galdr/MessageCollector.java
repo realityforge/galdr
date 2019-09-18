ca8828a8a7861a8c36a9e61cbafb36f19255a1af
Peter Donald <peter@realityforge.org>
2019-09-18 21:10:39 +1000

    Upgrade the version of braincheck

--- core/src/test/java/galdr/MessageCollector.java
+++ core/src/test/java/galdr/MessageCollector.java
@@ -2,63 +2,22 @@ package galdr;
 
 import java.io.File;
 import javax.annotation.Nonnull;
+import org.realityforge.braincheck.AbstractTestNGMessageCollector;
 import org.realityforge.braincheck.GuardMessageCollector;
-import org.testng.ITestContext;
-import org.testng.ITestListener;
-import org.testng.ITestResult;
-import org.testng.TestListenerAdapter;
 import static org.testng.Assert.*;
 
 public final class MessageCollector
-  extends TestListenerAdapter
-  implements ITestListener
+  extends AbstractTestNGMessageCollector
 {
-  @Nonnull
-  private final GuardMessageCollector _messages = createCollector();
-
   @Override
-  public void onTestStart( @Nonnull final ITestResult result )
-  {
-    if ( shouldCheckDiagnosticMessages() )
-    {
-      _messages.onTestStart();
-    }
-  }
-
-  @Override
-  public void onTestSuccess( @Nonnull final ITestResult result )
-  {
-    if ( shouldCheckDiagnosticMessages() )
-    {
-      _messages.onTestComplete();
-    }
-  }
-
-  @Override
-  public void onStart( @Nonnull final ITestContext context )
-  {
-    if ( shouldCheckDiagnosticMessages() )
-    {
-      _messages.onTestSuiteStart();
-    }
-  }
-
-  @Override
-  public void onFinish( @Nonnull final ITestContext context )
-  {
-    if ( 0 == context.getFailedTests().size() && shouldCheckDiagnosticMessages() )
-    {
-      _messages.onTestSuiteComplete();
-    }
-  }
-
-  private boolean shouldCheckDiagnosticMessages()
+  protected boolean shouldCheckDiagnosticMessages()
   {
     return System.getProperty( "galdr.check_diagnostic_messages", "true" ).equals( "true" );
   }
 
   @Nonnull
-  private GuardMessageCollector createCollector()
+  @Override
+  protected GuardMessageCollector createCollector()
   {
     final boolean saveIfChanged = "true".equals( System.getProperty( "galdr.output_fixture_data", "false" ) );
     final String fixtureDir = System.getProperty( "galdr.diagnostic_messages_file" );
