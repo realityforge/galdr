a0cf6e201ce92f16507306523bed5f74d06eddde
Peter Donald <peter@realityforge.org>
2019-09-19 08:51:08 +1000

    Add tests around toString and friends

--- core/src/test/java/galdr/WorldTest.java
+++ core/src/test/java/galdr/WorldTest.java
@@ -16,6 +16,18 @@ public class WorldTest
   }
 
   @Test
+  public void basicConstruct()
+  {
+    final World world1 = Galdr.world( "Foo" ).build();
+    final World world2 = Galdr.world().build();
+
+    assertEquals( world1.getName(), "Foo" );
+    assertEquals( world1.toString(), "World[Foo]" );
+    assertEquals( world2.getName(), "World@1" );
+    assertEquals( world2.toString(), "World[World@1]" );
+  }
+
+  @Test
   public void getComponentByType()
   {
     final World world = Galdr.world().component( Component1.class, Component1::new ).build();
