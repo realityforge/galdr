2e59ab4b5a0abe32620c9a354625b0285f56bf1e
Peter Donald <peter@realityforge.org>
2019-09-19 22:12:24 +1000

    Add tests for ProcessorStage

---
+++ core/src/test/java/galdr/ProcessorStageTest.java
@@ -0,0 +1,148 @@
+package galdr;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class ProcessorStageTest
+  extends AbstractTest
+{
+  public static class MyProcessor
+    extends Processor
+  {
+    @Nonnull
+    final String _key;
+    @Nonnull
+    final List<String> _trace;
+
+    MyProcessor( @Nonnull final String key, @Nonnull final List<String> trace )
+    {
+      _key = Objects.requireNonNull( key );
+      _trace = Objects.requireNonNull( trace );
+    }
+
+    @Override
+    protected void process( final int delta )
+    {
+      _trace.add( _key + ":" + delta );
+      assertEquals( world().getName(), "MyWorld" );
+    }
+  }
+
+  public static class MyFaultyProcessor
+    extends MyProcessor
+  {
+    MyFaultyProcessor( @Nonnull final String key, @Nonnull final List<String> trace )
+    {
+      super( key, trace );
+    }
+
+    @Override
+    protected void process( final int delta )
+    {
+      _trace.add( _key + ":!!Error!!:" + delta );
+      throw new IllegalStateException( "Blah!" );
+    }
+  }
+
+  @Test
+  public void basicOperation()
+  {
+    final List<String> trace = new ArrayList<>();
+    final World world = Galdr.world( "MyWorld" )
+      .stage( "MyStage",
+              new MyProcessor( "A", trace ),
+              new MyProcessor( "B", trace ),
+              new MyProcessor( "C", trace ) )
+      .build();
+
+    final ProcessorStage stage = world.getStageByName( "MyStage" );
+
+    assertEquals( stage.getName(), "MyStage" );
+    assertEquals( stage.toString(), "ProcessorStage[MyStage]" );
+
+    stage.process( 2 );
+
+    assertEquals( String.join( ",", trace ), "A:2,B:2,C:2" );
+
+    stage.process( 3 );
+    stage.process( 7 );
+
+    assertEquals( String.join( ",", trace ), "A:2,B:2,C:2,A:3,B:3,C:3,A:7,B:7,C:7" );
+  }
+
+  @Test
+  public void basicOperation_withArrayNotCopied()
+  {
+    GaldrTestUtil.disableCopyArraysPassedToConstructors();
+
+    final List<String> trace = new ArrayList<>();
+    final World world = Galdr.world( "MyWorld" )
+      .stage( "MyStage",
+              new MyProcessor( "A", trace ),
+              new MyProcessor( "B", trace ),
+              new MyProcessor( "C", trace ) )
+      .build();
+
+    final ProcessorStage stage = world.getStageByName( "MyStage" );
+
+    assertEquals( stage.getName(), "MyStage" );
+    assertEquals( stage.toString(), "ProcessorStage[MyStage]" );
+
+    stage.process( 2 );
+
+    assertEquals( String.join( ",", trace ), "A:2,B:2,C:2" );
+
+    stage.process( 3 );
+    stage.process( 7 );
+
+    assertEquals( String.join( ",", trace ), "A:2,B:2,C:2,A:3,B:3,C:3,A:7,B:7,C:7" );
+  }
+
+  @Test
+  public void toString_test()
+  {
+    final List<String> trace = new ArrayList<>();
+    final World world = Galdr.world( "MyWorld" )
+      .stage( "MyStage",
+              new MyProcessor( "A", trace ),
+              new MyProcessor( "B", trace ),
+              new MyProcessor( "C", trace ) )
+      .build();
+
+    final ProcessorStage stage = world.getStageByName( "MyStage" );
+    assertEquals( stage.toString(), "ProcessorStage[MyStage]" );
+
+    GaldrTestUtil.disableDebugToString();
+
+    assertNotEquals( stage.toString(), "ProcessorStage[MyStage]" );
+  }
+
+  @Test
+  public void processorGeneratesError()
+  {
+    final List<String> trace = new ArrayList<>();
+    final World world = Galdr.world( "MyWorld" )
+      .stage( "MyStage",
+              new MyProcessor( "A", trace ),
+              new MyFaultyProcessor( "B", trace ),
+              new MyProcessor( "C", trace ) )
+      .build();
+
+    world.addErrorHandler( ( stage, processor, throwable ) ->
+                             trace.add( "Stage: " + stage.getName() +
+                                        ", Processor: " + processor.getName() +
+                                        ", Error: " + throwable.toString() ) );
+    final ProcessorStage stage = world.getStageByName( "MyStage" );
+
+    assertEquals( stage.getName(), "MyStage" );
+    assertEquals( stage.toString(), "ProcessorStage[MyStage]" );
+
+    stage.process( 2 );
+
+    assertEquals( String.join( ",", trace ), "A:2,B:!!Error!!:2,Stage: MyStage, Processor: MyFaultyProcessor, Error: java.lang.IllegalStateException: Blah!,C:2" );
+  }
+}
