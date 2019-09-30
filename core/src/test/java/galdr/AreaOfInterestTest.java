5598607ac1107e0fa7af66f7f55d440be3beccd1
Peter Donald <peter@realityforge.org>
2019-09-30 20:57:45 +1000

    Add tests around hashCode() and equals()

--- core/src/test/java/galdr/AreaOfInterestTest.java
+++ core/src/test/java/galdr/AreaOfInterestTest.java
@@ -46,4 +46,30 @@ public class AreaOfInterestTest
 
     assertDefaultToString( areaOfInterest );
   }
+
+  @Test
+  public void hash_and_equals()
+  {
+    final BitSet set1 = new BitSet();
+    final BitSet set2 = new BitSet();
+    final BitSet set3 = new BitSet();
+    final BitSet set4 = new BitSet();
+
+    set1.set( 1 );
+    set2.set( 2 );
+    set3.set( 3 );
+    set4.set( 4 );
+
+    final AreaOfInterest aoi1 = new AreaOfInterest( set1, set2, set3 );
+    final AreaOfInterest aoi2 = new AreaOfInterest( set1, set2, set3 );
+    final AreaOfInterest aoi3 = new AreaOfInterest( set1, set2, set4 );
+
+    assertEquals( aoi1.hashCode(), aoi1.hashCode() );
+    assertEquals( aoi1.hashCode(), aoi2.hashCode() );
+    assertNotEquals( aoi1.hashCode(), aoi3.hashCode() );
+
+    assertEquals( aoi1, aoi1 );
+    assertEquals( aoi1, aoi2 );
+    assertNotEquals( aoi1, aoi3 );
+  }
 }
