e1e6333ddaae6694fc686f74799e68b42d9b6158
Peter Donald <peter@realityforge.org>
2019-09-30 21:28:11 +1000

    Extract helper method

--- core/src/test/java/galdr/AreaOfInterestTest.java
+++ core/src/test/java/galdr/AreaOfInterestTest.java
@@ -1,6 +1,7 @@
 package galdr;
 
 import java.util.BitSet;
+import javax.annotation.Nonnull;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
@@ -47,23 +48,13 @@ public class AreaOfInterestTest
   @Test
   public void toString_test()
   {
-    final BitSet all = new BitSet( 16 );
-    final BitSet one = new BitSet( 16 );
-    final BitSet exclude = new BitSet( 16 );
-
-    all.set( 0 );
-    all.set( 1 );
-    all.set( 5 );
-    all.set( 12 );
-
-    one.set( 2 );
-    all.set( 4 );
-
-    exclude.set( 14 );
+    final BitSet all = set( 0, 1, 5, 12 );
+    final BitSet one = set( 2, 4 );
+    final BitSet exclude = set( 14 );
 
     final AreaOfInterest areaOfInterest = new AreaOfInterest( all, one, exclude );
 
-    assertEquals( areaOfInterest.toString(), "AreaOfInterest[all={0, 1, 4, 5, 12},one={2},exclude={14}]" );
+    assertEquals( areaOfInterest.toString(), "AreaOfInterest[all={0, 1, 5, 12},one={2, 4},exclude={14}]" );
 
     GaldrTestUtil.disableDebugToString();
 
@@ -73,15 +64,10 @@ public class AreaOfInterestTest
   @Test
   public void hash_and_equals()
   {
-    final BitSet set1 = new BitSet();
-    final BitSet set2 = new BitSet();
-    final BitSet set3 = new BitSet();
-    final BitSet set4 = new BitSet();
-
-    set1.set( 1 );
-    set2.set( 2 );
-    set3.set( 3 );
-    set4.set( 4 );
+    final BitSet set1 = set( 1 );
+    final BitSet set2 = set( 2 );
+    final BitSet set3 = set( 3 );
+    final BitSet set4 = set( 4 );
 
     final AreaOfInterest aoi1 = new AreaOfInterest( set1, set2, set3 );
     final AreaOfInterest aoi2 = new AreaOfInterest( set1, set2, set3 );
@@ -95,5 +81,16 @@ public class AreaOfInterestTest
     assertEquals( aoi1, aoi2 );
     assertNotEquals( aoi1, aoi3 );
     assertNotEquals( aoi1, "" );
+  }
+
+  @Nonnull
+  private BitSet set( @Nonnull final int... bits )
+  {
+    final BitSet set = new BitSet();
+    for ( final int bit : bits )
+    {
+      set.set( bit );
+    }
+    return set;
   }
 }
