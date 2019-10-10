d44eabb7383ce6176bfea1bd672a9093ad00d4a3
Peter Donald <peter@realityforge.org>
2019-10-11 09:07:55 +1100

    Eliminate the name on processor

--- core/src/main/java/galdr/Processor.java
+++ core/src/main/java/galdr/Processor.java
@@ -1,62 +1,18 @@
 package galdr;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import static org.realityforge.braincheck.Guards.*;
-
 /**
  * Base processor class.
  */
 public abstract class Processor
 {
-  /**
-   * A human consumable name for Processor. It must be non-null if {@link Galdr#areNamesEnabled()} returns
-   * true and <tt>null</tt> otherwise.
-   */
-  @Nullable
-  private final String _name;
-
-  protected Processor()
-  {
-    this( null );
-  }
-
-  protected Processor( @Nullable final String name )
-  {
-    if ( Galdr.shouldCheckApiInvariants() )
-    {
-      apiInvariant( () -> Galdr.areNamesEnabled() || null == name,
-                    () -> "Galdr-0052: Processor passed a name '" + name + "' but Galdr.areNamesEnabled() is false" );
-    }
-    _name = Galdr.areNamesEnabled() ? null == name ? getClass().getSimpleName() : name : null;
-  }
-
   protected abstract void process( int delta );
-
-  /**
-   * Return the human readable name of the Processor.
-   * This method should NOT be invoked unless {@link Galdr#areNamesEnabled()} returns <code>true</code>.
-   *
-   * @return the human readable name of the Processor.
-   */
-  @Nonnull
-  public final String getName()
-  {
-    if ( Galdr.shouldCheckApiInvariants() )
-    {
-      apiInvariant( Galdr::areNamesEnabled,
-                    () -> "Galdr-0004: Processor.getName() invoked when Galdr.areNamesEnabled() returns false" );
-    }
-    assert null != _name;
-    return _name;
-  }
 
   @Override
   public String toString()
   {
     if ( Galdr.areDebugToStringMethodsEnabled() )
     {
-      return "Processor[" + getName() + "]";
+      return "Processor[" + getClass().getSimpleName() + "]";
     }
     else
     {
--- core/src/test/java/galdr/ProcessorTest.java
+++ core/src/test/java/galdr/ProcessorTest.java
@@ -1,6 +1,5 @@
 package galdr;
 
-import javax.annotation.Nullable;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
@@ -12,39 +11,11 @@ public class ProcessorTest
   {
     private int _lastDelta;
 
-    MyProcessor()
-    {
-    }
-
-    MyProcessor( @Nullable final String name )
-    {
-      super( name );
-    }
-
     @Override
     protected void process( final int delta )
     {
       _lastDelta = delta;
     }
-  }
-
-  @Test
-  public void construct()
-  {
-    final Worlds.Builder builder = Worlds.world();
-    // Cache the processor in var
-    final Processor processor = new MyProcessor();
-    builder.stage( randomString(), processor ).build();
-
-    assertEquals( processor.getName(), "MyProcessor" );
-  }
-
-  @Test
-  public void construct_namedPassedButNamesDisabled()
-  {
-    GaldrTestUtil.disableNames();
-    assertInvariantFailure( () -> new MyProcessor( "MyName" ),
-                            "Galdr-0052: Processor passed a name 'MyName' but Galdr.areNamesEnabled() is false" );
   }
 
   @Test
@@ -60,22 +31,6 @@ public class ProcessorTest
     GaldrTestUtil.disableDebugToString();
 
     assertDefaultToString( processor );
-  }
-
-  @Test
-  public void getName()
-  {
-    final Worlds.Builder builder = Worlds.world();
-    // Cache the processor in var
-    final Processor processor = new MyProcessor();
-    builder.stage( randomString(), processor ).build();
-
-    assertEquals( processor.getName(), "MyProcessor" );
-
-    GaldrTestUtil.disableNames();
-
-    assertInvariantFailure( processor::getName,
-                            "Galdr-0004: Processor.getName() invoked when Galdr.areNamesEnabled() returns false" );
   }
 
   @Test
