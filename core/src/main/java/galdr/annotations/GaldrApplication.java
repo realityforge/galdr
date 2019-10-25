15818b56d466764cbafc8ea8a376aae211076bc6
Peter Donald <peter@realityforge.org>
2019-10-25 13:48:40 +1100

    Add annotation use to mark applications

---
+++ core/src/main/java/galdr/annotations/GaldrApplication.java
@@ -0,0 +1,24 @@
+package galdr.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation used to mark a class that is processed by the Galdr annotation processor and is used to
+ * instantiate the world.
+ *
+ * <p>The type that is annotated with this annotation must comply with the additional constraints:</p>
+ * <ul>
+ * <li>Must be a class</li>
+ * <li>Must be abstract</li>
+ * <li>Must not be final</li>
+ * <li>Must not be a non-static nested class</li>
+ * <li>Must have at least one method annotated with {@link StageRef} or {@link WorldRef}</li>
+ * </ul>
+ */
+@Documented
+@Target( ElementType.TYPE )
+public @interface GaldrApplication
+{
+}
