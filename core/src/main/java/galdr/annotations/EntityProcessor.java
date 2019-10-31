4aebf4114bd32b7131e1949b241fbdbcea4e73ca
Peter Donald <peter@realityforge.org>
2019-10-31 13:22:08 +1100

    Add some javadocs

--- core/src/main/java/galdr/annotations/EntityProcessor.java
+++ core/src/main/java/galdr/annotations/EntityProcessor.java
@@ -7,11 +7,14 @@ import javax.annotation.Nonnull;
 
 /**
  * Annotation to identify method invoked to process entities.
+ * The method can have one or two parameters. If there is one parameter then the parameter contains the
+ * entityId to be processed. If there are two parameters then the first is the delta parameter passed to
+ * the {@link galdr.ProcessorStage#process(int)} method that results in this method being invoked.
  *
  * <p>The method that is annotated with this must comply with the additional constraints:</p>
  * <ul>
  * <li>Must not be annotated with any other galdr annotation</li>
- * <li>Must have 1 integer parameters</li>
+ * <li>Must have either 1 or 2 integer parameters.</li>
  * <li>Must not return a value</li>
  * <li>Must not be private</li>
  * <li>Must not be static</li>
