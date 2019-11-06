9f83feddd4905acbb2ea0d2c685aab31b6eb57ad
Peter Donald <peter@realityforge.org>
2019-11-06 21:34:49 +1100

    Add constants for annotation names

---
+++ processor/src/main/java/galdr/processor/Constants.java
@@ -0,0 +1,25 @@
+package galdr.processor;
+
+final class Constants
+{
+  static final String COMPONENT_CLASSNAME = "galdr.annotations.Component";
+  static final String COMPONENT_MANAGER_REF_CLASSNAME = "galdr.annotations.ComponentManagerRef";
+  static final String ENTITY_PROCESSOR_CLASSNAME = "galdr.annotations.EntityProcessor";
+  static final String APPLICATION_CLASSNAME = "galdr.annotations.GaldrApplication";
+  static final String ON_ACTIVATE_CLASSNAME = "galdr.annotations.OnActivate";
+  static final String ON_DEACTIVATE_CLASSNAME = "galdr.annotations.OnDeactivate";
+  static final String PROCESSOR_CLASSNAME = "galdr.annotations.Processor";
+  static final String STAGE_CLASSNAME = "galdr.annotations.Stage";
+  static final String SUB_SYSTEM_CLASSNAME = "galdr.annotations.SubSystem";
+  static final String SUB_SYSTEM_NAME_REF_CLASSNAME = "galdr.annotations.SubSystemNameRef";
+  static final String WORLD_REF_CLASSNAME = "galdr.annotations.WorldRef";
+  static final String NONNULL_CLASSNAME = "javax.annotation.Nonnull";
+  static final String NULLABLE_CLASSNAME = "javax.annotation.Nullable";
+  static final String DEPRECATED_CLASSNAME = "java.lang.Deprecated";
+  static final String GENERATED_CLASSNAME = "javax.annotation.Generated";
+  static final String JAVA9_GENERATED_CLASSNAME = "javax.annotation.processing.Generated";
+
+  private Constants()
+  {
+  }
+}
