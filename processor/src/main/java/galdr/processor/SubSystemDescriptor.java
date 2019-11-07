de22cc66a2afed18cd21c362de64faae78da7c1d
Peter Donald <peter@realityforge.org>
2019-11-07 21:30:38 +1100

    Start to model SubSystem metadata

---
+++ processor/src/main/java/galdr/processor/SubSystemDescriptor.java
@@ -0,0 +1,40 @@
+package galdr.processor;
+
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import javax.lang.model.element.TypeElement;
+
+final class SubSystemDescriptor
+{
+  @Nonnull
+  private final TypeElement _element;
+  @Nonnull
+  private final String _name;
+  private final int _priority;
+
+  SubSystemDescriptor( @Nonnull final TypeElement element,
+                       @Nonnull final String name,
+                       final int priority )
+  {
+    _element = Objects.requireNonNull( element );
+    _name = Objects.requireNonNull( name );
+    _priority = priority;
+  }
+
+  @Nonnull
+  TypeElement getElement()
+  {
+    return _element;
+  }
+
+  @Nonnull
+  String getName()
+  {
+    return _name;
+  }
+
+  int getPriority()
+  {
+    return _priority;
+  }
+}
