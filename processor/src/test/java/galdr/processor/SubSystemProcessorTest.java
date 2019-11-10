ceb5179873b478f7d33b5dc37af83f1985df95d1
Peter Donald <peter@realityforge.org>
2019-11-10 21:45:26 +1100

    Whitespace

--- processor/src/test/java/galdr/processor/SubSystemProcessorTest.java
+++ processor/src/test/java/galdr/processor/SubSystemProcessorTest.java
@@ -36,7 +36,7 @@ public class SubSystemProcessorTest
     throws Exception
   {
     assertSuccessfulCompile( "input/com/example/StaticInnerClassSubSystem.java",
-                             "expected/com/example/StaticInnerClassSubSystem_Galdr_Foo.java");
+                             "expected/com/example/StaticInnerClassSubSystem_Galdr_Foo.java" );
   }
 
   @DataProvider( name = "failedCompiles" )
@@ -47,16 +47,20 @@ public class SubSystemProcessorTest
         new Object[]{ "com.example.ctor.MultipleCtorSubSystem", "XXXXXXXX" },
         new Object[]{ "com.example.ctor.PrivateAccessCtorSubSystem", "XXXXXXXX" },
         new Object[]{ "com.example.name_ref.ConcreteNameRefSubSystem", "@NameRef target must be abstract" },
-        new Object[]{ "com.example.name_ref.ParameterizedNameRefSubSystem", "@NameRef target must not have any parameters" },
+        new Object[]{ "com.example.name_ref.ParameterizedNameRefSubSystem",
+                      "@NameRef target must not have any parameters" },
         new Object[]{ "com.example.name_ref.PrivateNameRefSubSystem", "@NameRef target must be abstract" },
         new Object[]{ "com.example.name_ref.StaticNameRefSubSystem", "@NameRef target must be abstract" },
-        new Object[]{ "com.example.name_ref.ThrowsExceptionNameRefSubSystem", "@NameRef target must not throw any exceptions" },
-        new Object[]{ "com.example.name_ref.UnreachableNameRefSubSystem", "@NameRef target must not be package access if the method is in a different package from the type annotated with the @GaldrApplication annotation" },
+        new Object[]{ "com.example.name_ref.ThrowsExceptionNameRefSubSystem",
+                      "@NameRef target must not throw any exceptions" },
+        new Object[]{ "com.example.name_ref.UnreachableNameRefSubSystem",
+                      "@NameRef target must not be package access if the method is in a different package from the type annotated with the @GaldrApplication annotation" },
         new Object[]{ "com.example.ConcreteSubSystem", "@SubSystem target must be abstract" },
         new Object[]{ "com.example.EnumSubSystem", "@SubSystem target must be a class" },
         new Object[]{ "com.example.InterfaceSubSystem", "@SubSystem target must be a class" },
-        new Object[]{ "com.example.NonStaticInnerClassSubSystem", "@SubSystem target must not be a non-static nested class" },
-      };
+        new Object[]{ "com.example.NonStaticInnerClassSubSystem",
+                      "@SubSystem target must not be a non-static nested class" },
+        };
   }
 
   @Test( dataProvider = "failedCompiles" )
