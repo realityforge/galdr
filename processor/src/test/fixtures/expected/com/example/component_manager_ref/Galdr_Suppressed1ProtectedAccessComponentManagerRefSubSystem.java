306f3ed316557b0bef6c83311ec79bc733bcd57e
Peter Donald <peter@realityforge.org>
2019-11-14 20:17:13 +1100

    Add expected fixtures

---
+++ processor/src/test/fixtures/expected/com/example/component_manager_ref/Galdr_Suppressed1ProtectedAccessComponentManagerRefSubSystem.java
@@ -0,0 +1,66 @@
+package com.example.component_manager_ref;
+
+import galdr.ComponentManager;
+import galdr.Galdr;
+import galdr.World;
+import galdr.internal.PostConstructFn;
+import java.util.Objects;
+import javax.annotation.Generated;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+@Generated("galdr.processor.SubSystemProcessor")
+public final class Galdr_Suppressed1ProtectedAccessComponentManagerRefSubSystem implements PostConstructFn {
+  @Nonnull
+  private final EnhancedSubSystem _subsystem = new EnhancedSubSystem( this );
+
+  @Nonnull
+  private World $galdr$_getWorld() {
+    return World.current();
+  }
+
+  @Override
+  public void postConstruct() {
+    _subsystem.$galdr$_postConstruct();
+  }
+
+  private static final class EnhancedSubSystem extends Suppressed1ProtectedAccessComponentManagerRefSubSystem {
+    @Nonnull
+    private final Galdr_Suppressed1ProtectedAccessComponentManagerRefSubSystem $galdr$_outer;
+
+    @Nullable
+    private ComponentManager<Suppressed1ProtectedAccessComponentManagerRefSubSystem.MyComponent> $galdrc$_cm;
+
+    private EnhancedSubSystem(
+        @Nonnull final Galdr_Suppressed1ProtectedAccessComponentManagerRefSubSystem outer) {
+      $galdr$_outer = Objects.requireNonNull( outer );
+    }
+
+    @Override
+    @Nonnull
+    protected ComponentManager<Suppressed1ProtectedAccessComponentManagerRefSubSystem.MyComponent> cm(
+        ) {
+      assert null != $galdrc$_cm;
+      return $galdrc$_cm;
+    }
+
+    private void $galdr$_postConstruct() {
+      $galdrc$_cm = $galdr$_outer.$galdr$_getWorld().getComponentByType( Suppressed1ProtectedAccessComponentManagerRefSubSystem.MyComponent.class );
+    }
+
+    @Nonnull
+    private String $galdr$_getName() {
+      return "Suppressed1ProtectedAccessComponentManagerRefSubSystem";
+    }
+
+    @Nonnull
+    @Override
+    public String toString() {
+      if ( Galdr.areDebugToStringMethodsEnabled() ) {
+        return "SubSystem[" + $galdr$_getName() + "]";
+      } else {
+        return super.toString();
+      }
+    }
+  }
+}
