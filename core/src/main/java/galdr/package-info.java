83b2d05c0c6c55546b6bee30869be1d62cca0cf5
Peter Donald <peter@realityforge.org>
2019-11-16 15:33:15 +1100

    Add assertion so that no clinits in generated classes are present

--- core/src/main/java/galdr/package-info.java
+++ core/src/main/java/galdr/package-info.java
@@ -2,6 +2,7 @@
  * Core Galdr primitives.
  */
 @OmitPattern( symbol = "^\\$clinit$" )
+@OmitPattern( type = "^.*\\.Galdr_[^.]+$", symbol = "^\\$clinit$" )
 package galdr;
 
 import grim.annotations.OmitPattern;
