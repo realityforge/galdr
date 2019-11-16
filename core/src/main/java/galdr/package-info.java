5618eb388799d1bbc3fb90385a07735d6773950c
Peter Donald <peter@realityforge.org>
2019-11-16 15:33:37 +1100

    Add assertion to ensure toString in generated classes are stripped unless debug_to_string enabled

--- core/src/main/java/galdr/package-info.java
+++ core/src/main/java/galdr/package-info.java
@@ -3,6 +3,7 @@
  */
 @OmitPattern( symbol = "^\\$clinit$" )
 @OmitPattern( type = "^.*\\.Galdr_[^.]+$", symbol = "^\\$clinit$" )
+@OmitPattern( type = "^.*\\.Galdr_[^.]+$", symbol = "^toString$", unless = "galdr.debug_to_string" )
 package galdr;
 
 import grim.annotations.OmitPattern;
