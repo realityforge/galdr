e2ab8f248e937827d91f9fe0228bea8d03d12e93
Peter Donald <peter@realityforge.org>
2019-12-24 17:06:52 +1100

    Optimize imports

--- processor/src/main/java/galdr/processor/ApplicationDescriptor.java
+++ processor/src/main/java/galdr/processor/ApplicationDescriptor.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.Nonnull;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.type.TypeMirror;
 
 final class ApplicationDescriptor
   extends AbstractDescriptor
