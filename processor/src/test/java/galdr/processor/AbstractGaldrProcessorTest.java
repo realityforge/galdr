605705e18899a334ccc8507efc0ef2ba7b786487
Peter Donald <peter@realityforge.org>
2020-01-06 08:46:11 +1100

    Use helper method from parent class

--- processor/src/test/java/galdr/processor/AbstractGaldrProcessorTest.java
+++ processor/src/test/java/galdr/processor/AbstractGaldrProcessorTest.java
@@ -1,7 +1,5 @@
 package galdr.processor;
 
-import java.util.ArrayList;
-import java.util.List;
 import javax.annotation.Nonnull;
 import org.realityforge.proton.qa.AbstractProcessorTest;
 
@@ -11,29 +9,7 @@ abstract class AbstractGaldrProcessorTest
   void assertSuccessfulCompile( @Nonnull final String classname )
     throws Exception
   {
-    assertSuccessfulCompile( classname, deriveExpectedOutputs( classname ) );
-  }
-
-  @Nonnull
-  private String[] deriveExpectedOutputs( @Nonnull final String classname )
-  {
-    final String[] elements = classname.contains( "." ) ? classname.split( "\\." ) : new String[]{ classname };
-    final StringBuilder subsystem = new StringBuilder();
-    subsystem.append( "expected" );
-    for ( int i = 0; i < elements.length; i++ )
-    {
-      subsystem.append( '/' );
-      final boolean isLastElement = i == elements.length - 1;
-      if ( isLastElement )
-      {
-        subsystem.append( "Galdr_" );
-      }
-      subsystem.append( elements[ i ] );
-    }
-    subsystem.append( ".java" );
-    final List<String> expectedOutputs = new ArrayList<>();
-    expectedOutputs.add( subsystem.toString() );
-    return expectedOutputs.toArray( new String[ 0 ] );
+    assertSuccessfulCompile( classname, toFilename( "expected", classname, "Galdr_", ".java" ) );
   }
 
   @Nonnull
